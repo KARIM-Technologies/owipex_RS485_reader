f14c6f50e86172c0eaaa2f02b53cff44947edb97
Sayed Amir Karim <s.karim@wer.ag>
2024-10-29 14:56:00 +0100

    read timing issues

--- modbus_manager.py
+++ modbus_manager.py
@@ -2,7 +2,7 @@ import struct
 import serial
 import crcmod.predefined
 from threading import Thread
-from time import sleep
+import time
 
 class ModbusClient:
     def __init__(self, device_manager, device_id):
@@ -27,6 +27,7 @@ class DeviceManager:
         )
         self.devices = {}
         self.last_read_values = {}
+        self._lock = threading.Lock()
 
     def add_device(self, device_id):
         self.devices[device_id] = ModbusClient(self, device_id)
@@ -35,23 +36,31 @@ class DeviceManager:
     def get_device(self, device_id):
         return self.devices.get(device_id)
 
+    def _send_and_receive(self, message, expected_length):
+        with self._lock:
+            self.ser.reset_input_buffer()
+            self.ser.write(message)
+            time.sleep(0.1)  # Give device time to respond
+            response = self.ser.read(expected_length)
+            return response
+
     def read_register(self, device_id, start_address, register_count, data_format):
         function_code = 0x03
         message = struct.pack('>B B H H', device_id, function_code, start_address, register_count)
         crc16 = crcmod.predefined.mkPredefinedCrcFun('modbus')(message)
         message += struct.pack('<H', crc16)
 
-        self.ser.write(message)
-        response = self.ser.read(100)
+        # Expected response length: 1 byte address + 1 byte function code + 1 byte length + 4 bytes data + 2 bytes CRC
+        expected_length = 9
+        response = self._send_and_receive(message, expected_length)
 
-        if len(response) < 2:
-            print('Received response is shorter than expected')
+        if len(response) < expected_length:
             return self.last_read_values.get((device_id, start_address), None)
 
         received_crc = struct.unpack('<H', response[-2:])[0]
         calculated_crc = crcmod.predefined.mkPredefinedCrcFun('modbus')(response[:-2])
+        
         if received_crc != calculated_crc:
-            print('CRC error in response')
             return self.last_read_values.get((device_id, start_address), None)
 
         data = response[3:-2]
@@ -62,9 +71,32 @@ class DeviceManager:
             self.last_read_values[(device_id, start_address)] = value
             return value
         except struct.error:
-            print(f'Error decoding data from device {device_id}')
             return self.last_read_values.get((device_id, start_address), None)
 
     def read_radar_sensor(self, device_id, register_address):
         """Special method for reading radar sensor data with unsigned short format"""
-        return self.read_register(device_id, register_address, 1, data_format='>H')
+        function_code = 0x03
+        message = struct.pack('>B B H H', device_id, function_code, register_address, 1)
+        crc16 = crcmod.predefined.mkPredefinedCrcFun('modbus')(message)
+        message += struct.pack('<H', crc16)
+
+        # Expected response length for radar sensor: 1 byte address + 1 byte function code + 1 byte length + 2 bytes data + 2 bytes CRC
+        expected_length = 7
+        response = self._send_and_receive(message, expected_length)
+
+        if len(response) < expected_length:
+            return self.last_read_values.get((device_id, register_address), None)
+
+        received_crc = struct.unpack('<H', response[-2:])[0]
+        calculated_crc = crcmod.predefined.mkPredefinedCrcFun('modbus')(response[:-2])
+        
+        if received_crc != calculated_crc:
+            return self.last_read_values.get((device_id, register_address), None)
+
+        data = response[3:-2]
+        try:
+            value = struct.unpack('>H', data)[0]
+            self.last_read_values[(device_id, register_address)] = value
+            return value
+        except struct.error:
+            return self.last_read_values.get((device_id, register_address), None)
