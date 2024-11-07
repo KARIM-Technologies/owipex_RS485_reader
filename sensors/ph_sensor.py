83f4805066c495d6543a5250683b11919ebbe619
KarimSayedAmir <136378589+KarimSayedAmir@users.noreply.github.com>
2024-11-07 13:15:35 +0100

    correction reading value

--- sensors/ph_sensor.py
+++ sensors/ph_sensor.py
@@ -7,14 +7,14 @@ class PHSensor(SensorBase):
     def read_data(self):
         """Read pH sensor data"""
         try:
-            # Lese PH-Wert (Register 0x0001, 2 Register für Float)
-            ph_value = self.device.read_register(start_address=0x0001, register_count=2, data_format='>f')
+            # Lese PH-Wert (Register 0x0001, 2 Register)
+            ph_value = self.device.read_register(start_address=0x0001, register_count=2)
             if ph_value is None:
                 self.logger.error(f"Fehler beim Lesen des PH-Werts von Gerät {self.device_id}")
                 return None
                 
-            # Lese Temperatur (Register 0x0003, 2 Register für Float)
-            temperature = self.device.read_register(start_address=0x0003, register_count=2, data_format='>f')
+            # Lese Temperatur (Register 0x0003, 2 Register)
+            temperature = self.device.read_register(start_address=0x0003, register_count=2)
             if temperature is None:
                 self.logger.error(f"Fehler beim Lesen der Temperatur von Gerät {self.device_id}")
                 return None
--- sensors/turbidity_sensor.py
+++ sensors/turbidity_sensor.py
@@ -7,14 +7,14 @@ class TurbiditySensor(SensorBase):
     def read_data(self):
         """Read turbidity sensor data"""
         try:
-            # Lese Trübung (Register 0x0001, 2 Register für Float)
-            turbidity = self.device.read_register(start_address=0x0001, register_count=2, data_format='>f')
+            # Lese Trübung (Register 0x0001, 2 Register)
+            turbidity = self.device.read_register(start_address=0x0001, register_count=2)
             if turbidity is None:
                 self.logger.error(f"Fehler beim Lesen der Trübung von Gerät {self.device_id}")
                 return None
                 
-            # Lese Temperatur (Register 0x0003, 2 Register für Float)
-            temperature = self.device.read_register(start_address=0x0003, register_count=2, data_format='>f')
+            # Lese Temperatur (Register 0x0003, 2 Register)
+            temperature = self.device.read_register(start_address=0x0003, register_count=2)
             if temperature is None:
                 self.logger.error(f"Fehler beim Lesen der Temperatur von Gerät {self.device_id}")
                 return None
